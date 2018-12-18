2e3fec8768200d95a441c390e07ec5ac1644320f
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-12-18 10:18:49 +0530

    made more changes to design and added more images

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
 
 <!-- Basic Page Needs
 ================================================== -->
-<title>Callie - Blog Template</title>
+<title>101 Musicals Academy of Music</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
